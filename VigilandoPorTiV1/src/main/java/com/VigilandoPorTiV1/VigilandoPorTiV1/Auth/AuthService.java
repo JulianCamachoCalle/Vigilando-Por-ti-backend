923fade5d35c018088ec7bcb7e309a109d509c42
Julian Fernando Camacho Calle <u22242383@utp.edu.pe>
2024-11-09 18:28:01 -0500

    BACKEND LOGIN AND REGISTER

--- VigilandoPorTiV1/src/main/java/com/VigilandoPorTiV1/VigilandoPorTiV1/Auth/AuthService.java
+++ VigilandoPorTiV1/src/main/java/com/VigilandoPorTiV1/VigilandoPorTiV1/Auth/AuthService.java
@@ -7,7 +7,6 @@ import com.VigilandoPorTiV1.VigilandoPorTiV1.User.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -22,7 +21,7 @@ public class AuthService {
 
     public AuthResponse login(LoginRequest request) {
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
-        UserDetails user = userRepository.findByUsername(request.getUsername()).orElseThrow();
+        User user = userRepository.findByUsername(request.getUsername()).orElseThrow();
         String token = jwtService.getToken(user);
         return AuthResponse.builder()
                 .token(token)
