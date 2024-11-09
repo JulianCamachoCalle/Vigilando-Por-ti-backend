8624e72a532a760e4693e2c01029b0cccbf969f9
Julian Fernando Camacho Calle <u22242383@utp.edu.pe>
2024-11-08 22:41:20 -0500

    Backend

---
+++ VigilandoPorTiV1/src/main/java/com/VigilandoPorTiV1/VigilandoPorTiV1/VigilandoPorTiV1Application.java
@@ -0,0 +1,13 @@
+package com.VigilandoPorTiV1.VigilandoPorTiV1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VigilandoPorTiV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VigilandoPorTiV1Application.class, args);
+	}
+
+}
---
+++ VigilandoPorTiV1/src/test/java/com/VigilandoPorTiV1/VigilandoPorTiV1/VigilandoPorTiV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VigilandoPorTiV1.VigilandoPorTiV1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VigilandoPorTiV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
